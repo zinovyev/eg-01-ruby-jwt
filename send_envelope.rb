5b707dfc237eb144a73a03ed2a8b29ad34e0e568
Shadi Massalha <shady.massalha@gmail.com>
2018-11-21 11:03:52 +0200

    modify send envelope to include all the logic

--- send_envelope.rb
+++ send_envelope.rb
@@ -37,36 +37,49 @@ class SendEnvelope < ExampleBase
 
   def sendEnvelope
     check_token
-    envelope = createEnvelope
-    envelope_api = DocuSign_eSign::EnvelopesApi.new(@@api_client)
-    result = envelope_api.create_envelope(@@account_id, envelope)
-    result
-  end
+    envelope = DocuSign_eSign::EnvelopeDefinition.new
+    envelope.email_subject = "Please sign this document sent from Ruby SDK"
 
-  def readContent(filename)
-    File.binread(File.join('data', filename))
-  end
+    # doc1 = create_document_from_template("1", "Order acknowledgement", "html", @@ENVELOPE_1_DOCUMENT_1)
+    doc1 = DocuSign_eSign::Document.new
+    doc1.document_base64 = Base64.encode64(@@ENVELOPE_1_DOCUMENT_1)
+    doc1.name = "Order acknowledgement"
+    doc1.file_extension = "html"
+    doc1.document_id = "1"
 
-  def createEnvelope
-    envelope_definition = DocuSign_eSign::EnvelopeDefinition.new
-    envelope_definition.email_subject = "Please sign this document sent from Ruby SDK"
+    doc2 = DocuSign_eSign::Document.new
 
-    doc1 = create_document_from_template("1", "Order acknowledgement", "html", @@ENVELOPE_1_DOCUMENT_1)
-    doc2 = create_document_from_template("2", "Battle Plan", "docx", readContent(@@DOC_2_DOCX))
-    doc3 = create_document_from_template("3", "Lorem Ipsum", "pdf", readContent(@@DOC_3_PDF))
+    doc2.document_base64 = Base64.encode64(File.binread(File.join('data', @@DOC_2_DOCX)))
+    doc2.name = "Battle Plan"
+    doc2.file_extension = "docx"
+    doc2.document_id = "2"
+
+    doc3 = DocuSign_eSign::Document.new
+    doc3.document_base64 = Base64.encode64(File.binread(File.join('data', @@DOC_3_PDF)))
+    doc3.name = "Lorem Ipsum"
+    doc3.file_extension = "pdf"
+    doc3.document_id = "3"
 
     # The order in the docs array determines the order in the envelope
-    envelope_definition.documents = [doc1, doc2, doc3]
+    envelope.documents = [doc1, doc2, doc3]
     # create a signer recipient to sign the document, identified by name and email
     # We're setting the parameters via the object creation
-    signer1 = create_signer
+    signer1 = DocuSign_eSign::Signer.new
+    signer1.email = DSConfig.signer_email
+    signer1.name = DSConfig.signer_name
+    signer1.recipient_id = "1"
+    signer1.routing_order = "1"
     # routingOrder (lower means earlier) determines the order of deliveries
     # to the recipients. Parallel routing order is supported by using the
     # same integer as the order for two or more recipients.
 
     # create a cc recipient to receive a copy of the documents, identified by name and email
     # We're setting the parameters via setters
-    cc1 = create_carbon_copy()
+    cc1 = DocuSign_eSign::CarbonCopy.new
+    cc1.email = DSConfig.cc_email
+    cc1.name = DSConfig.cc_name
+    cc1.routing_order = "2"
+    cc1.recipient_id = "2"
     # Create signHere fields (also known as tabs) on the documents,
     # We're using anchor (autoPlace) positioning
     #
@@ -74,78 +87,32 @@ class SendEnvelope < ExampleBase
     # documents for matching anchor strings. So the
     # sign_here_2 tab will be used in both document 2 and 3 since they
     # use the same anchor string for their "signer 1" tabs.
-    sign_here1 = create_sign_here("**signature_1**", "pixels", "20", "10")
-    sign_here2 = create_sign_here("/sn1/", "pixels", "20", "10")
-    # Tabs are set per recipient / signer
-    set_signer_tabs(signer1, [sign_here1, sign_here2])
-    # Add the recipients to the envelope object
-    recipients = create_recipients(signer1, cc1)
-    envelope_definition.recipients = recipients
-    # Request that the envelope be sent by setting |status| to "sent".
-    # To request that the envelope be created as a draft, set to "created"
-    envelope_definition.status = "sent"
-    envelope_definition
-  end
+    sign_here1 = DocuSign_eSign::SignHere.new
+    sign_here1.anchor_string = "**signature_1**"
+    sign_here1.anchor_units = "pixels"
+    sign_here1.anchor_x_offset = "20"
+    sign_here1.anchor_y_offset = "10"
 
-  def create_recipients(signer1, cc1)
-    # code here
+    sign_here2 = DocuSign_eSign::SignHere.new
+    sign_here2.anchor_string = "/sn1/"
+    sign_here2.anchor_units = "pixels"
+    sign_here2.anchor_x_offset = "20"
+    sign_here2.anchor_y_offset = "10"
+    # Tabs are set per recipient / signer
+    tabs = DocuSign_eSign::Tabs.new
+    tabs.sign_here_tabs = [sign_here1, sign_here2]
+    signer1.tabs = tabs
+    # Add the recipients to the envelope object
     recipients = DocuSign_eSign::Recipients.new
     recipients.signers = [signer1]
     recipients.carbon_copies = [cc1]
-    recipients
+    envelope.recipients = recipients
+    # Request that the envelope be sent by setting |status| to "sent".
+    # To request that the envelope be created as a draft, set to "created"
+    envelope.status = "sent"
+    envelope_api = DocuSign_eSign::EnvelopesApi.new(@@api_client)
+    result = envelope_api.create_envelope(@@account_id, envelope)
+    result
   end
 
-  def set_signer_tabs(signer1, signers)
-    # code here
-    tabs = DocuSign_eSign::Tabs.new
-    tabs.sign_here_tabs = signers
-    signer1.tabs = tabs
-  end
-
-  def create_sign_here(anchor_pattern, anchor_units, anchor_x_offset, anchor_y_offset)
-    sign_here = DocuSign_eSign::SignHere.new
-    sign_here.anchor_string = anchor_pattern
-    sign_here.anchor_units = anchor_units
-    sign_here.anchor_x_offset = anchor_x_offset
-    sign_here.anchor_y_offset = anchor_y_offset
-    sign_here
-  end
-
-  def create_carbon_copy
-    # code here
-    cc = DocuSign_eSign::CarbonCopy.new
-    cc.email = DSConfig.cc_email
-    cc.name = DSConfig.cc_name
-    cc.routing_order = "2"
-    cc.recipient_id = "2"
-    cc
-  end
-
-  def create_signer
-    # code here
-    signer = DocuSign_eSign::Signer.new
-    signer.email = DSConfig.signer_email
-    signer.name = DSConfig.signer_name
-    signer.recipient_id = "1"
-    signer.routing_order = "1"
-    signer
-  end
-
-
-  def create_document_from_template(id, name, file_extension, content)
-    # code here
-    document = DocuSign_eSign::Document.new
-
-    document.document_base64 = Base64.encode64(content)
-
-    # can be different from actual file name
-    document.name = name
-    # Source data format. Signed docs are always pdf.
-    document.file_extension = file_extension
-    # a label used to reference the doc
-    document.document_id = id
-
-    return document
-
-  end
 end
