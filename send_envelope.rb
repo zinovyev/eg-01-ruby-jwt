d70ef8337c54e9c0745071a232698069c575ed90
larry-kluger <larry.kluger@docusign.com>
2018-12-20 11:07:29 +0200

    Update send_envelope.rb

--- send_envelope.rb
+++ send_envelope.rb
@@ -40,47 +40,47 @@ class SendEnvelope < ExampleBase
     check_token
 
     # Create the envelope request
-    envelope = DocuSign_eSign::EnvelopeDefinition.new
-    envelope.email_subject = "Please sign this document sent from Ruby SDK"
+    envelope = DocuSign_eSign::EnvelopeDefinition.new({
+      :emailSubject => "Please sign this document sent from Ruby SDK"})
 
-    doc1 = DocuSign_eSign::Document.new
-    doc1.document_base64 = Base64.encode64(@@ENVELOPE_1_DOCUMENT_1)
-    doc1.name = "Order acknowledgement"
-    doc1.file_extension = "html"
-    doc1.document_id = "1"
+    doc1 = DocuSign_eSign::Document.new({
+      :documentBase64 => Base64.encode64(@@ENVELOPE_1_DOCUMENT_1),
+      :name => "Order acknowledgement",
+      :fileExtension => "html",
+      :documentId => "1"})
 
-    doc2 = DocuSign_eSign::Document.new
-    doc2.document_base64 = Base64.encode64(File.binread(File.join('data', @@DOC_2_DOCX)))
-    doc2.name = "Battle Plan"
-    doc2.file_extension = "docx"
-    doc2.document_id = "2"
+    doc2 = DocuSign_eSign::Document.new({
+      :documentBase64 => Base64.encode64(File.binread(File.join('data', @@DOC_2_DOCX))),
+      :name => "Battle Plan",
+      :fileExtension => "docx",
+      :documentId => "2"})
 
-
-    doc3 = DocuSign_eSign::Document.new
-    doc3.document_base64 = Base64.encode64(File.binread(File.join('data', @@DOC_3_PDF)))
-    doc3.name = "Lorem Ipsum"
-    doc3.file_extension = "pdf"
-    doc3.document_id = "3"
+    doc3 = DocuSign_eSign::Document.new({
+      :documentBase64 => Base64.encode64(File.binread(File.join('data', @@DOC_3_PDF))),
+      :name => "Lorem Ipsum",
+      :file_Extension => "pdf",
+      :documentId => "3"})
 
     # The order in the docs array determines the order in the envelope
     envelope.documents = [doc1, doc2, doc3]
     # create a signer recipient to sign the document, identified by name and email
-    signer1 = DocuSign_eSign::Signer.new
-    signer1.email = DSConfig.signer_email
-    signer1.name = DSConfig.signer_name
-    signer1.recipient_id = "1"
-    signer1.routing_order = "1"
+    signer1 = DocuSign_eSign::Signer.new({
+      :email => DSConfig.signer_email,
+      :name => DSConfig.signer_name,
+      :recipientId => "1",
+      :routingOrder => "1"})
     # routingOrder (lower means earlier) determines the order of deliveries
     # to the recipients. Parallel routing order is supported by using the
     # same integer as the order for two or more recipients.
 
     # create a cc recipient to receive a copy of the documents, identified by name and email
     # We're setting the parameters via setters
-    cc1 = DocuSign_eSign::CarbonCopy.new
-    cc1.email = DSConfig.cc_email
-    cc1.name = DSConfig.cc_name
-    cc1.routing_order = "2"
-    cc1.recipient_id = "2"
+    cc1 = DocuSign_eSign::CarbonCopy.new({
+      :email => DSConfig.cc_email,
+      :name => DSConfig.cc_name,
+      :routingOrder => "2",
+      :recipientId => "2"})
+
     # Create signHere fields (also known as tabs) on the documents,
     # We're using anchor (autoPlace) positioning
     #
@@ -88,17 +88,17 @@ class SendEnvelope < ExampleBase
     # documents for matching anchor strings. So the
     # sign_here_2 tab will be used in both document 2 and 3 since they
     # use the same anchor string for their "signer 1" tabs.
-    sign_here1 = DocuSign_eSign::SignHere.new
-    sign_here1.anchor_string = "**signature_1**"
-    sign_here1.anchor_units = "pixels"
-    sign_here1.anchor_x_offset = "20"
-    sign_here1.anchor_y_offset = "10"
+    sign_here1 = DocuSign_eSign::SignHere.new({
+      :anchorString => "**signature_1**",
+      :anchorUnits => "pixels",
+      :anchorXOffset => "20",
+      :anchorYOffset => "10"})
 
-    sign_here2 = DocuSign_eSign::SignHere.new
-    sign_here2.anchor_string = "/sn1/"
-    sign_here2.anchor_units = "pixels"
-    sign_here2.anchor_x_offset = "20"
-    sign_here2.anchor_y_offset = "10"
+    sign_here2 = DocuSign_eSign::SignHere.new({
+      :anchorString => "/sn1/",
+      :anchorUnits => "pixels",
+      :anchorXOffset => "20",
+      :anchorYOffset => "10"})
 
     # Tabs are set per recipient / signer
     tabs = DocuSign_eSign::Tabs.new
@@ -106,9 +106,9 @@ class SendEnvelope < ExampleBase
     signer1.tabs = tabs
 
     # Add the recipients to the envelope object
-    recipients = DocuSign_eSign::Recipients.new
-    recipients.signers = [signer1]
-    recipients.carbon_copies = [cc1]
+    recipients = DocuSign_eSign::Recipients.new({
+      :signers => [signer1],
+      :carbonCopies => [cc1]})
     envelope.recipients = recipients
 
     # Request that the envelope be sent by setting |status| to "sent".
